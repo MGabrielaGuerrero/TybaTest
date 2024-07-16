500dc2584c6b96cc743169c73ea89c9b706efce1
MGabrielaGuerrero <ggmaria@unicauca.edu.co>
2024-07-16 14:55:19 -0500

    [FEATURE] run with docker

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userNonRepeat = require('../middlewares/usernonrepeat.middleware');
 const { userAdd, singIn, signOut } = require('../controllers/user.controller');
 const singUpValidateSchema = require('../middlewares/singUpValidateSchema.middleware');
 const singInValidateSchema = require('../middlewares/signInValidateSchema.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
+const userNonRepeat = require('../middlewares/userNonRepeat.middleware'); 
 
 router.post('/singUp', singUpValidateSchema, userNonRepeat,  userAdd)
 
