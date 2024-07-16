c1dbf9d10b3e69a6c0b78f015da58034c84e079d
MGabrielaGuerrero <ggmaria@unicauca.edu.co>
2024-07-16 12:28:19 -0500

    [FEATURE] add registrer transaction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { logTransaction } = require("./transaction.controller");
 
 const userAdd = async (req, res) => {
   console.log("🚀 ~ userAdd ~ req:", req.body.data)
@@ -9,6 +10,11 @@ const userAdd = async (req, res) => {
     const user = new User({ userName, mail, password });
     await user.save();
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Registrar la transacción
+    await logTransaction(
+      userName,
+      'SIGNUP'
+    );
     res.status(201).json({ token });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -17,15 +23,20 @@ const userAdd = async (req, res) => {
 
 const singIn = async (req, res) => {
   console.log("🚀 ~ singIn ~ req.body.data:", req.body.data)
-  const { email, password } = req.body.data;
+  const { userName, password } = req.body.data;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ userName });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    // Registrar la transacción
+    await logTransaction(
+      userName,
+      'SIGNIN'
+    );
     res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -34,7 +45,14 @@ const singIn = async (req, res) => {
 };
 
 const signOut = async (req, res) => {
+  console.log("🚀 ~ signOut ~ req:", req.body.data)
+  const { userName } = req.body.data;
   // invalidar el token
+  // Registrar la transacción
+  await logTransaction(
+    userName,
+    'SIGNOUT'
+  );
   res.json({ msg:  'Logged out successfully' });
 };
 
--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
 const transactionModel = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    type: {
-        type: String,
-        enum: ['RESTAURANT_SEARCH', "SIGNIN", "SIGNOUT", "SINGUPD"],
-        required: true
-    },
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ['RESTAURANT_SEARCH', "SIGNIN", "SIGNOUT", "SIGNUP"],
+        required: true
     }
 });
 
